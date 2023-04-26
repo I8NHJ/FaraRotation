e04eda952a33051d5455c7b78c8a860cb267bf8a
Massimo 'Max' Mucci <max.i8nhj@gmail.com>
2023-04-26 17:02:11 -0500

    Update FaraRotation.ino

--- FaraRotation.ino
+++ FaraRotation.ino
@@ -1,13 +1,15 @@
 #define DEBUG
 #define CODE_VERSION "2023.04.25.01 N5NHJ"
-#define CONTROL_PORT_SERIAL_PORT_CLASS Serial2
-#define NEXTION_PORT_SERIAL_PORT_CLASS Serial3
+#define CONTROL_PORT_SERIAL_PORT_CLASS Serial_
+// #define CONTROL_PORT_SERIAL_PORT_CLASS HardwareSerial
+#define NEXTION_PORT_SERIAL_PORT_CLASS HardwareSerial
 
 #include "FaraRotation_Pins.h"
 #include "FaraRotation_Settings.h"
 
 /*-------- VARIABLES --------*/
-CONTROL_PORT_SERIAL_PORT_CLASS * control_port;
+CONTROL_PORT_SERIAL_PORT_CLASS *control_port;
+NEXTION_PORT_SERIAL_PORT_CLASS *nextion_port;
 byte incoming_nextion_byte = 0;
 unsigned long last_nextion_receive_time = 0;
 byte control_port_buffer[COMMAND_BUFFER_SIZE];
@@ -23,19 +25,19 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  check_nextion_port();
+//   check_nextion_port();
 }
 
 /*-------- SUBROUTINES --------*/
 void initialize_serial(){
-  control_port = CONTROL_PORT;
+  control_port = CONTROL_PORT_MAPPED_TO;
   control_port->begin(CONTROL_PORT_BAUD_RATE);
   control_port->flush();
     #ifdef DEBUG
       control_port->println (F("App started"));
       control_port->flush();
     #endif
-  nextion_port = NEXTION_PORT;
+  nextion_port = NEXTION_PORT_MAPPED_TO;
   nextion_port->begin(NEXTION_PORT_BAUD_RATE);
   nextion_port->flush();
 
@@ -60,12 +62,10 @@ void clear_nextion_buffer(){
 
 void check_nextion_port(){
   if (nextion_port->available()){
-    incoming_nextion_byte = nextion_port->read();
-    last_nextion_receive_time = millis();
+     incoming_nextion_byte = nextion_port->read();
+     last_nextion_receive_time = millis();
   }
   if ((incoming_nextion_byte > 96) && (incoming_nextion_byte < 123)) {  // uppercase it
-    incoming_snextion_byte = incoming_nextion_byte - 32;
+    incoming_nextion_byte = incoming_nextion_byte - 32;
   }  
 }
-
-
