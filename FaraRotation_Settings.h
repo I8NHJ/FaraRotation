ec3e8739d7c282ce1533322a3a22e4255854e1ac
Massimo 'Max' Mucci <max.i8nhj@gmail.com>
2023-04-26 17:01:10 -0500

    Update FaraRotation_Settings.h

--- FaraRotation_Settings.h
+++ FaraRotation_Settings.h
@@ -1,8 +1,8 @@
 // Serial Port Settings
-#define CONTROL_PORT &Serial     // change this line to map the control port to a different serial port (Serial1, Serial2, etc.)
+#define CONTROL_PORT_MAPPED_TO &Serial
 #define CONTROL_PORT_BAUD_RATE 9600
 // Nextion Serial Port Settings
-#define NEXTION_PORT &Serial3
+#define NEXTION_PORT_MAPPED_TO &Serial1
 #define NEXTION_PORT_BAUD_RATE 115200
 
 #define COMMAND_BUFFER_SIZE 50 
