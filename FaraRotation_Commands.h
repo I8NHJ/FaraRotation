1e2810d8f1da4d2d626ea2881edb77186c902a4a
Massimo 'Max' Mucci <max.i8nhj@gmail.com>
2023-04-28 21:15:47 +0000

    Add files via upload

---
+++ FaraRotation_Commands.h
@@ -0,0 +1,9 @@
+// NEXTION COMMANDS DEFINITION
+const char STOP[] = "*0";
+const char ROTATE_RX_ANTENNA_CW[]  = "*RRX1";
+const char ROTATE_RX_ANTENNA_CCW[] = "*RRX2";
+const char ROTATE_TX_ANTENNA_CW[]  = "*RTX1";
+const char ROTATE_TX_ANTENNA_CCW[] = "*RTX2";
+const char SYNC_TX_TO_RX[]         = "*RTX3";
+const char SYNC_RX_AND_TX[]        = "*RRXTX";
+
