f66ae98bb73edba47697855c707f00165fc9bf51
Massimo 'Max' Mucci <max.i8nhj@gmail.com>
2023-04-28 16:15:18 -0500

    Update FaraRotation_Settings.h

--- FaraRotation_Settings.h
+++ FaraRotation_Settings.h
@@ -1,8 +1,12 @@
 // Serial Port Settings
+// Leonardo Eth defines Class Serial (USB) and Serial1 (Pins 0,1)
 #define CONTROL_PORT_MAPPED_TO &Serial
 #define CONTROL_PORT_BAUD_RATE 9600
+#define CONTROL_PORT_READ_RATE 250
+
 // Nextion Serial Port Settings
 #define NEXTION_PORT_MAPPED_TO &Serial1
-#define NEXTION_PORT_BAUD_RATE 115200
+#define NEXTION_PORT_BAUD_RATE 9600
+#define NEXTION_PORT_READ_RATE 250
 
 #define COMMAND_BUFFER_SIZE 50 
