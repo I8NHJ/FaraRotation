d9e1151eea64ca160f294d02243768b2173c3c88
Massimo 'Max' Mucci <max.i8nhj@gmail.com>
2023-05-02 16:56:41 -0500

    Update FaraRotation_Commands.h

--- FaraRotation_Commands.h
+++ FaraRotation_Commands.h
@@ -1,9 +1,21 @@
-// NEXTION COMMANDS DEFINITION
-const char STOP[] = "*0";
+// NEXTION CONTROL STRINGS
+const char STOP[]  = "*0";
 const char ROTATE_RX_ANTENNA_CW[]  = "*RRX1";
 const char ROTATE_RX_ANTENNA_CCW[] = "*RRX2";
 const char ROTATE_TX_ANTENNA_CW[]  = "*RTX1";
 const char ROTATE_TX_ANTENNA_CCW[] = "*RTX2";
-const char SYNC_TX_TO_RX[]         = "*RTX3";
+const char SYNC_TX_2_RX[]          = "*RTX3";
 const char SYNC_RX_AND_TX[]        = "*RRXTX";
+const char CALIBRATE_RX_ANTENNA_CW[]  = "*CRX1";
+const char CALIBRATE_RX_ANTENNA_CCW[] = "*CRX2";
+const char CALIBRATE_TX_ANTENNA_CW[]  = "*CTX1";
+const char CALIBRATE_TX_ANTENNA_CCW[] = "*CTX2";
 
+// NEXTION COMMAND STRINGS
+const char NEXTION_POST_COMMAND[] = "ÿÿÿ";
+
+// COMMAND ENUMERATORS
+const byte ROTATE_RX_ANTENNA_CW_ENU  = 1;
+const byte ROTATE_RX_ANTENNA_CCW_ENU = 2;
+const byte ROTATE_TX_ANTENNA_CW_ENU  = 3;
+const byte ROTATE_TX_ANTENNA_CCW_ENU = 4;
