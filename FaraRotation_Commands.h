5f599bf21b81b48dfff912aae333b595ebc2bc41
Massimo 'Max' Mucci <max.i8nhj@gmail.com>
2023-05-04 16:57:31 -0500

    Update FaraRotation_Commands.h

--- FaraRotation_Commands.h
+++ FaraRotation_Commands.h
@@ -1,15 +1,19 @@
 // NEXTION CONTROL STRINGS
 const char STOP[]  = "*0";
-const char ROTATE_RX_ANTENNA_CW[]  = "*RRX1";
-const char ROTATE_RX_ANTENNA_CCW[] = "*RRX2";
-const char ROTATE_TX_ANTENNA_CW[]  = "*RTX1";
-const char ROTATE_TX_ANTENNA_CCW[] = "*RTX2";
-const char SYNC_TX_2_RX[]          = "*RTX3";
-const char SYNC_RX_AND_TX[]        = "*RRXTX";
-const char CALIBRATE_RX_ANTENNA_CW[]  = "*CRX1";
-const char CALIBRATE_RX_ANTENNA_CCW[] = "*CRX2";
-const char CALIBRATE_TX_ANTENNA_CW[]  = "*CTX1";
-const char CALIBRATE_TX_ANTENNA_CCW[] = "*CTX2";
+const char ROTATE_RX_ANTENNA_CW[]      = "*RRX1";
+const char ROTATE_RX_ANTENNA_CCW[]     = "*RRX2";
+const char ROTATE_TX_ANTENNA_CW[]      = "*RTX1";
+const char ROTATE_TX_ANTENNA_CCW[]     = "*RTX2";
+const char SYNC_TX_2_RX[]              = "*RTX3";
+const char ROTATE_RX_AND_TX_ENABLE[]   = "*RRXTX1";
+const char ROTATE_RX_AND_TX_DISABLE[]  = "*RRXTX0";
+const char CALIBRATE_RX_ANTENNA_CW[]   = "*CRX1";
+const char CALIBRATE_RX_ANTENNA_CCW[]  = "*CRX2";
+const char CALIBRATE_TX_ANTENNA_CW[]   = "*CTX1";
+const char CALIBRATE_TX_ANTENNA_CCW[]  = "*CTX2";
+const char SAVE_CALLSIGN_TO_EEPROM[]   = "*SC";
+const char SAVE_GRID_TO_EEPROM[]       = "*SG";
+const char SEND_CONFIG[]               = "*CFG";
 
 // NEXTION COMMAND STRINGS
 const char NEXTION_POST_COMMAND[] = "ÿÿÿ";
